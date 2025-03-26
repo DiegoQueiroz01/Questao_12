216a713a029b401483002871edd3ae0279c6733c
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-03-26 15:58:46 -0300

    Código com ajustes finais;

--- src/Exemplar.java
+++ src/Exemplar.java
@@ -3,14 +3,15 @@ public class Exemplar {
         private String titulo;
         private String autores;
         private boolean aptoEmprestimo;
-        private int emprestimo;
+        private int emprestimos;
+        private static int totalDeEmprestimos = 0;
 
         public Exemplar(int tombo, String titulo, String autores, boolean aptoEmprestimo, int emprestimo){ //construtor da classe
             this.tombo = tombo;
             this.autores = autores;
             this.titulo = titulo;
             this.aptoEmprestimo = aptoEmprestimo;
-            this.emprestimo = emprestimo;
+            this.emprestimos = 0;
         }
 
     public int getTombo() {
@@ -30,7 +31,7 @@ public class Exemplar {
     }
 
     public int getEmprestimo() {
-        return emprestimo;
+        return emprestimos;
     }
     // habilita se o exemplar pode ser emprestado.
     public void habilitarEmprestimo(){
@@ -42,11 +43,29 @@ public class Exemplar {
         this.aptoEmprestimo = false;
         System.out.println("Exemplar " + titulo + " (Tombo " + tombo + "), não está apto para o empréstimo.");
     }
+    // mostra se um exemplar foi emprestado com sucesso ou possui falha no momento do empréstimo.
+    public void registrarEmprestimo(){
+        if(aptoEmprestimo){
+            aptoEmprestimo = false;
+            emprestimos++;
+            System.out.println("Exemplar " + titulo + " (Tombo " + tombo + "), foi emprestado com sucesso.");
+        }
+        else{
+            System.out.println("Exemplar " + titulo + " (Tombo " + tombo + "), não está apto para empréstimo no momento.");
+        }
+    }
+    public int getEmprestimos(){
+        return emprestimos;
+    }
+    public static int getTotalDeEmprestimos(){
+        return totalDeEmprestimos;
+    }
     // atualização da quantidade de empréstimo de um exemplar, em uma unidade.
     public void adicionarEmprestimo(){
         if(this.aptoEmprestimo){ 
-            this.emprestimo += 1;
+            this.emprestimos += 1;
             // se o exemplar está apto a ser emprestado, acrescente um a quantidade de empréstimos.
         }
     }
+    
 }
--- src/ExemplarUtil01.java
+++ src/ExemplarUtil01.java
@@ -8,17 +8,13 @@ public class ExemplarUtil01 {
         Exemplar[] acervo = new Exemplar[MAX]; //array baseado na quantidade de exemplares definidos.
 
         for (int i = 0; i < MAX; i++){
-            //entrada de tombo do último exemplar.
+            //entrada de tombo do exemplar.
             System.out.printf("Tombo do exemplar %d: ", i + 1); //entrada de tombo do último exemplar.
             int tombo = scanner.nextInt();
-            scanner.nextInt();
 
-            //entrada de título do último exemplar.
-            System.out.printf("Título do exemplar %d: ", i + 1);
+            //entrada de título e autoria do exemplar.
+            System.out.printf("Título do exemplar e autoria %d: ", i + 1);
             String titulo = scanner.nextLine();
-
-            //entrada de nome dos autores do último exemplar.
-            System.out.printf("Autoria de exemplar %d: ", i + 1);
             String autoria = scanner.nextLine();
 
             //código que mostra se o exemplar está apto ou não para o emprestimo.
@@ -28,6 +24,54 @@ public class ExemplarUtil01 {
             //instanciação de enésimo objeto da classe Exemplar e armazenamento de referência em enésimo índice do array
             acervo[i] = new Exemplar(tombo, titulo, autoria, aptoEmprestimo, tombo);
         }
+        int opcao;
+        do { //menu com opções e situaçãoes
+            System.out.println("\nMenu");
+            System.out.println("\nEscolha uma opção:\n");
+            System.out.println("1 - Registrar empréstimo.");
+            System.out.println("2 - Exibir número total de empréstimos.");
+            System.out.println("3 - Exibir exemplar com maior número de empréstimos.");
+            System.out.println("4 - Encerrar aplicação.");
+            opcao = scanner.nextInt();
+            scanner.nextInt();
+
+            switch (opcao){
+        case 1:
+                //registra se um novo exemplar for empréstado.
+            System.out.println("\nRegistro de empréstimo de exemplar...");
+            System.out.println("Informe o código de tombo do exemplar desejado: ");
+            int tomboBuscar = scanner.nextInt();
+    
+            for(Exemplar exemplar : acervo){
+                if(exemplar != null && exemplar.isAptoEmprestimo()){
+                    exemplar.registrarEmprestimo();
+                    break;
+                }
+            }
+        case 2:
+            // exibe o exemplar com mais registros de empréstimo.
+            if (acervo.length > 0 && acervo[0] != null) {
+                System.out.println("\nTotal de empréstimos realizados: " + acervo[0].getTotalDeEmprestimos());
+            } else {
+                System.out.println("\nTotal de empréstimos realizados: 0");
+            }
+            break;
+        case 3:
+            int maxEmprestimos = 0;
+            for(Exemplar exemplar : acervo){
+            if(exemplar.getEmprestimos() > maxEmprestimos){
+                maxEmprestimos = exemplar.getEmprestimos();
+            }
+            System.out.println("\nExemplares com mais registros de empréstimo: ");
+            for(Exemplar ex : acervo){
+                if(ex.getEmprestimos() == maxEmprestimos && maxEmprestimos > 0){
+                    System.out.println("- " + ex.getTitulo() + "(Tombo " + ex.getTombo() + "), com " + maxEmprestimos + " empréstimos.");
+                }
+            }
+        }
+    }
+    } while (opcao != 4);
+            
         scanner.close(); //fecha o fluxo de operação de
     }
 }
